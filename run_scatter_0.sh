a84920f9fb7d365626dcf96db31e98f3a9c60952
Kris Thielemans <Kris.Thielemans@stir>
2015-10-25 22:33:46 +0000

    fix scatter scripts to no longer copy images files from obsolete location

--- run_scatter_0.sh
+++ run_scatter_0.sh
@@ -2,7 +2,6 @@ for g in 1 2 ; do
 cd working_folder/GATE${g}
 stir_subtract -s input_g${g} my_prompts_g${g}.hs my_randoms_g${g}.hs
 cp ../../EX_scatter/* . 
-cp ../*g${g}*v .
 endN=3 ATTEN_IMAGE=CTAC_g${g}.hv scatterpar=scatter_correction_350keV.par ./estimate_scatter.sh CTAC_g${g}.hv input_g${g}.hs scatter_template.hs my_acfs_g${g}.hs my_norm_g${g}.hs 
 cd ../.. 
 done
