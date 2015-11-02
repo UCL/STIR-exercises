1007f0abe8bf77046421498e9ef34cd3aa430c99
Kris Thielemans <Kris.Thielemans@stir>
2015-11-02 01:31:28 +0000

    added evaluation for randoms

---
+++ evaluate_randoms.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""
+Example script to serve as starting point for displaying the results of the brain reconstruction.
+
+The current script reads results from the simulation and displays them.
+
+Prerequisite:
+You should have executed the following on your command prompt
+    ./run_simulation_brain.sh
+    ./run_reconstruction_brain.sh
+    ./run_reconstruction_brain_MAP.sh
+    
+Author: Kris Thielemans
+"""
+#%% Initial imports
+import numpy
+import matplotlib.pyplot as plt
+import stir
+from stirextra import *
+import os
+import io
+#%% go to directory with input files
+# adapt this path to your situation (or start everything in the exercises directory)
+os.chdir('/home/stir/exercises')
+#%% change directory to where the output files are
+os.chdir('working_folder/randoms')
+#%% Define functions that read and write the .out files used by STIR
+"""
+  STIR uses a Mathematica-type of text-format for arrays, where {} are used for every row.
+  Here we define a function that gets rid of the braces and then uses numpy.fromstring
+  to read the comma-separated values into a numpy array.
+  
+  Warning: the next implementation only works for 1D arrays
+"""
+def readSinglesFile(filename):
+    f=io.open(filename,'r');
+    lines=f.readlines();
+    f.close();
+    array=numpy.array([]);
+    for l in lines:
+        l=l.replace('}','').replace('{','').strip();
+        if len(l)!=0:
+            a=numpy.fromstring(l,sep=',');
+            array=numpy.append(array,a);
+    return array;
+    
+"""
+  Write a 1D array as a STIR text-file for 2D arrays, as appropriate for the STIR normalisation code
+"""
+def writeSinglesFile(filename, singles):
+    numpy.savetxt(filename, singles.reshape(1,singles.shape[0]), delimiter=',', fmt='%g',header='{{', footer='}}', comments='');
+
+#%% read original and estimated singles
+mean_singles=readSinglesFile('true_singles_eff_1_1.out');
+estimated_singles=readSinglesFile('estimated_singles_eff_1_10.out');
+fansums=readSinglesFile('fansums_for_my_randoms.dat');
+#%% read sinograms
+mean_randoms=to_numpy(stir.ProjData.read_from_file('mean_randoms.hs'));
+noisy_randoms=to_numpy(stir.ProjData.read_from_file('my_randoms.hs'));
+estimated_randoms=to_numpy(stir.ProjData.read_from_file('estimated_randoms.hs'));
+#%% Plot sinograms
+maxforplot=mean_randoms.max()*1.3;
+slice=0;
+
+plt.figure()
+ax=plt.subplot(1,3,1);
+plt.imshow(mean_randoms[slice,:,:,]);
+plt.clim(0,maxforplot)
+ax.set_title('mean of randoms');
+plt.axis('off');
+plt.colorbar();
+
+ax=plt.subplot(1,3,2);
+plt.imshow(noisy_randoms[slice,:,:,]);
+plt.clim(0,maxforplot);
+ax.set_title('noisy randoms\n(i.e. delayeds)');
+plt.axis('off');
+plt.colorbar();
+
+ax=plt.subplot(1,3,3);
+plt.imshow(estimated_randoms[slice,:,:,]);
+plt.clim(0,maxforplot);
+ax.set_title('estimated randoms');
+plt.axis('off');
+plt.colorbar();
+
+#%% plot singles themselves
+plt.figure()
+plt.hold(True)
+plt.plot(mean_singles,'b.-')
+plt.plot(estimated_singles,'g.-')
+plt.legend(('original singles','estimated singles'));
+#%% Plot the ratio of the fansums with estimated
+plt.figure()
+plt.plot(fansums/estimated_singles,'r.-')
+plt.legend(('fansums / estimated'));
+
+#%% Extended exercise: set some singles to zero
+# Here we modify the mean of the singles (i.e. the simulation input)
+# by setting some values to zero (e.g. to simulate a defective block)
+# 
+# After this, run the run_randoms.sh script again to get new sinograms and estimates.
+# You can the just repeat the evaluation by executing this evaluation script again).
+mean_singles[10:18]=0;
+writeSinglesFile('true_singles_eff_1_1.out',mean_singles)
--- run_randoms.sh
+++ run_randoms.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 mkdir -p working_folder
 mkdir -p working_folder/randoms
-cp EX_randoms/* working_folder/randoms
+rsync -auv EX_randoms/* working_folder/randoms
 cd working_folder/randoms
-construct_randoms_from_singles true_randoms.hs true_singles template_sinogram_span1.hs 1
+construct_randoms_from_singles mean_randoms.hs true_singles template_sinogram_span1.hs 1
 
-poisson_noise my_randoms.hs true_randoms.hs 1 1
+poisson_noise my_randoms.hs mean_randoms.hs 1 1
 
 # generate file with text to tell next command that we don't want to display results,
 # but have output at every iteration
@@ -14,9 +14,9 @@ cat <<EOF > input_parameters.txt
 1
 1
 EOF
-find_ML_singles_from_delayed estimated_singles my_randoms.hs  5 < input_parameters.txt
+find_ML_singles_from_delayed estimated_singles my_randoms.hs  15 < input_parameters.txt
 
-construct_randoms_from_singles estimated_randoms.hs estimated_singles template_sinogram_span1.hs 5
+construct_randoms_from_singles estimated_randoms.hs estimated_singles my_randoms.hs 5
 
 
 cd ../..
