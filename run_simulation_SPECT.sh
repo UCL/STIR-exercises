08acf845e5782b0a3995d9b6f6255976cc5b1f69
Casper da Costa-Luis <casper.dcl@physics.org>
2017-11-17 23:24:17 +0000

    bugfixes (#1)
    * minor updates, requirements.sh

--- run_simulation_SPECT.sh
+++ run_simulation_SPECT.sh
@@ -1,8 +1,8 @@
-mkdir -p working_folder
+#! /bin/sh -e
+
 mkdir -p working_folder/single_slice_SPECT
 cp EX_simulation_single_slice_SPECT/* working_folder/single_slice_SPECT
 cd working_folder/single_slice_SPECT
 ./simulate_data.sh emission.hv CTAC.hv template_sinogram.hs
-
 
 echo DONE
--- run_simulation_single_slice.sh
+++ run_simulation_single_slice.sh
@@ -1,9 +1,9 @@
 #! /bin/sh -e
 
-mkdir -p working_folder
 mkdir -p working_folder/single_slice
 cp EX_simulation_single_slice/* working_folder/single_slice
 cd working_folder/single_slice
 ./simulate_data.sh emission.hv CTAC.hv template_sinogram.hs
 rm -f  *zero* *scatter*
+
 echo DONE
